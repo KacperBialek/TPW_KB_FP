771840effbcd8abe131c9c4acda7d70f6c8c3d24
Franciszek Pawlak <247756@edu.p.lodz.pl>
2024-03-06 16:46:36 +0100

    Initial Project

---
+++ Projekt/Projekt/Cow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsola
+{
+    public class Cow : Animal
+    {
+        private int obtained_milk;
+        public int Obtained_milk
+        { get; set; }
+        public Cow(int weight, string owner, gender animalgender, double price, int obtained_milk)
+        {
+            this.weight = weight;
+            this.owner = owner;
+            this.animalgender = animalgender;
+            this.price = price;
+            this.obtained_milk = obtained_milk;
+        }
+        public override double value()
+        { return obtained_milk * price; }
+    }
+}
